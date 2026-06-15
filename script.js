effd3efce8f3420cdbf5c856a3ef22166d9f7a88
Lydiab8 <152434512+Lydiab8@users.noreply.github.com>
2026-06-15 20:46:46 +0200

    Implement popup display on button click
    Add event listener to show popup on button click.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    const closeBtn = document.getElementById('btn-close');
+    const popup = document.getElementById('popup');
+
+    // Quand on clique sur le bouton, on ajoute la classe 'active' pour afficher le pop-up
+    closeBtn.addEventListener('click', () => {
+        popup.classList.add('active');
+    });
+});
