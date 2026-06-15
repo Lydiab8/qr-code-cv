62a5f6a1bb2876543a78f7a85df830920ab3191d
Lydiab8 <152434512+Lydiab8@users.noreply.github.com>
2026-06-15 21:46:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-
 document.addEventListener('DOMContentLoaded', () => {
+    const cvLink = document.getElementById('cv-link');
     const closeBtn = document.getElementById('btn-close');
     const popup = document.getElementById('popup');
 
-    // Quand on clique sur le bouton, on ajoute la classe 'active' pour afficher le pop-up
+    // 1. Quand on clique sur le bouton vert du CV
+    cvLink.addEventListener('click', () => {
+        // On attend un tout petit peu que le PDF s'ouvre, puis on fait apparaître le bouton bleu
+        setTimeout(() => {
+            closeBtn.classList.remove('hidden');
+        }, 500);
+    });
+
+    // 2. Quand on clique sur le bouton bleu "Terminer la visite"
     closeBtn.addEventListener('click', () => {
         popup.classList.add('active');
     });
